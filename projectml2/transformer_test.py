ccf5ec1f060207750cb04f4a99e6df88bb752f7e
Honst211 <270076582@qq.com>
2024-03-29 17:51:16 +0800

    projectml2 完成

---
+++ projectml2/transformer_test.py
@@ -0,0 +1,11 @@
+"""
+2. Transformer 模型
+- Transformer模型是如何组成的？
+- 你觉得Transformer最难是哪一部分，为什么？
+- 实现简易的Transformer
+"""
+
+"""
+Transformer 模型由多个相同的编码器（Encoder）和解码器（Decoder）组成，
+每个编码器和解码器都是由多层的自注意力机制（Self-Attention）和全连接前馈神经网络（Feed-Forward Neural Network）组成
+"""
